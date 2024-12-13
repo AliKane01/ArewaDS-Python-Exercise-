ad5cce38979b3004d9a271c1bfd49fb2568d9f92
Aliyu Kane <kanealiyu@gmail.com>
2024-12-13 22:58:28 +0100

    edited 02-Module-Variables.py

---
+++ 02_Module_Variables_builtin_functions/variables.py
@@ -0,0 +1,43 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Aliyu'
+last_name = 'Kane'
+full_name = 'Aliyu Kane'
+country = 'Nigeria'
+city = 'Sokoto'
+age = 39
+year = 1985
+is_married = True
+is_true = 'Yes' 
+is_light_on = 'Yes'
+first_name, Last_name, Full_name, Country, Age, Year, Is_married, Is_true = 'Aliyu', 'Kane', 'Aliyu_Kane', 'Nigeria', 'Sokoto', 39, 1985, 'True', 'Married', Yes
+
+
+# Excercise LLevel 2
+# Checking the data type
+print(len(first_name))
+print(len(last_name) < len(first_name))
+num_one, num_two = 5,4
+addition = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+floor_division = num_two // num_one
+
+
+
+# Import Math
+are_of_circle = math.pi * 30 ** 2
+circum_of_circle = math.pi * 2 * 30
+r + input("radius of circle: ")
+area = math.pi * (int(r) ** 2)
+name = input('Enter your first name:')
+last_name = input('enter your last name:')
+country = input('Enter your country:')
+age = input('Enter your age:')
+
+print(area_of_circle)
+
+help('keywords')
+
+
